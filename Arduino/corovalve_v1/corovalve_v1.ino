6edcc28ae4913b8733bdc1bcc46cb71806e216d7
EdgarPons <edgarponsforn@gmail.com>
2020-03-17 00:20:04 +0100

    1st Version

---
+++ Arduino/corovalve_v1/corovalve_v1.ino
@@ -0,0 +1,25 @@
+/*
+ * How to control stepper motor -  https://www.makerguides.com/l298n-stepper-motor-arduino-tutorial/
+ * 
+ * Install libraries: 
+ * ST7032_asukiaaa
+ * Accelstepper
+*/
+
+// INITIAL VARIABLES  //
+int volumen = 500 //Initial air volumen per cicle
+int cicles = 30 // Cicles per minute
+float tempdown = 0.5 // Duration of pressure
+
+int peet_min = 5 // Value of pressure min to PEET procedure
+int presure_max = 50 // Security value to stop pressing
+
+
+void setup() {
+
+}
+
+void loop() {
+
+  
+}
