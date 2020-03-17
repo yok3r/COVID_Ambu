508b71c16b6116d1752c9912ee8125f4aba9e66e
EdgarPons <edgarponsforn@gmail.com>
2020-03-17 17:41:00 +0100

    Update corovalve_v1.ino

--- Arduino/corovalve_v1/corovalve_v1.ino
+++ Arduino/corovalve_v1/corovalve_v1.ino
@@ -1,13 +1,9 @@
-/*
-   How to control stepper motor -  https://www.makerguides.com/l298n-stepper-motor-arduino-tutorial/
-   Install libraries:
-   ST7032_asukiaaa
-*/
+
 
 // LIBRARIES //
-#include <Stepper.h>  // Servo motor library
-#include <Servo.h> //TEMPORAL
-Servo myservo;  //TEMPORAL
+#define dirPin 2
+#define stepPin 3
+
 int led1 = 5;
 int led2 = 6;
 int endstopper = 7;
@@ -22,7 +18,7 @@ int valuepot2 = 0; // Breaths per minute
 int valuepot3 = 0; // Inale to exhale time ratio
 
 // PROTOTYPE ADJUSTS //
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+#define stepsPerRevolution 1  // change this to fit the number of steps per revolution
 
 // INITIAL VARIABLES  //
 int volumeMin = 50; //Initial air volume per cicle
@@ -44,9 +40,8 @@ int presure_max = 50; // Security value to stop pressing
 
 // OTHERS //
 // Buttons
-const int startbutton = 2;
-int speedServo = 100; // Default servo speed
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+const int startbutton = 4;
+
 int state = 4;
 int stepCount = 0; // number of steps the motor has taken
 int startState = 0; //Define the state, 1: start 0: stop to prepare the variables before starting
@@ -56,9 +51,10 @@ int pressureValue = 0; // Keep the pressure value
 int endstopperValue = 0;
 
 void setup() {
-  myservo.attach(9); //TEMPORAL
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+
   Serial.begin(9600);
-  myStepper.setSpeed(speedServo); // Set the motor speed
   pinMode(startbutton, INPUT);
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
@@ -67,32 +63,14 @@ void setup() {
 }
 
 void loop() {
-  // Read the value of 3 parameters
-  valuepot1 = analogRead(pot1);
-  valuepot2 = analogRead(pot2);
-  valuepot3 = analogRead(pot3);
-  endstopperValue = digitalRead(endstopper);
 
-  actualVolume = map(valuepot1, 20, 1000, volumeMin, volumeMax);
-  actualCicles = map(valuepot2, 20, 1000, speedMin, speedMax);
-  actualSpeed = map(valuepot3, 20, 1000, speedMin, speedMax);
-  
-  Serial.print("Volume: ");
-  Serial.print(actualVolume);
-  Serial.print(" |  Cicles: ");
-  Serial.print(actualCicles);
-  Serial.print(" |  Speed: ");
-  Serial.print(actualSpeed);
-  Serial.print(" |  Endstopper: ");
-  Serial.println(endstopperValue);
 
 
 
   currentMillis = millis();   // capture the latest value of millis()
   // Define the variables of new loop
   //Speed = fix * desired duration
-  speedServo = 100 ;
-  myStepper.setSpeed(speedServo);
+
   //Serial.println(currentMillis);
 
   if (startState == HIGH) { // Check to start
@@ -108,33 +86,49 @@ void loop() {
       ////  WAIT FOR NEXT CYCLE  ////
       //delaynow = (60000/cicles)-(cicles*2*(speedServo*volume)); // REPASAR
       delaynow = (60000 / actualCicles); // Basico para ir tirando
-      //Serial.println(delaynow);
+      Serial.println("Waiting");
       delay(delaynow);
       state = 1;
       break;
 
     case 1: // Motor down
+      digitalWrite(dirPin, HIGH); // Set the spinning direction clockwise:
 
-      //for (int i = 0; (i < volume) && (pressureValue <= presure_max)); i++) {
-      for (int i = 0; i < actualVolume; i++) {
-        //pressureValue = analogRead(pressure);
-        //myStepper.step(1);
-        digitalWrite(led1, HIGH);
-        delay(1);
+      for (int j = 0; j < 10; j++) { //Numero de veces que revisas las variables mientras corre el motor
+        // check variables
+        for (int i = 0; i < ((stepsPerRevolution / j)*actualVolume); i++) { //Divides el Stepsperrevolution por la J que son las veces que quieres revisar las variables.
+          // Move the motor
+          digitalWrite(stepPin, HIGH);
+          delayMicroseconds(2000);
+          digitalWrite(stepPin, LOW);
+          delayMicroseconds(2000);
+        }
+        Serial.print("Motor fase: ");
+        Serial.println(j);
       }
-      digitalWrite(led1, LOW);
       state = 2;
       break;
 
     case 2: // Motor up+
       //for (int i = 0; (i < actualVolume) && (endstopperValue == LOW)); i++) {
       //endstopperValue = analogRead(endstopper);
-      for (int i = 0; i < actualVolume; i++) {
-        //myStepper.step(-1);
-        digitalWrite(led2, HIGH);
-        delay(1);
+
+
+      digitalWrite(dirPin, LOW); // Set the spinning direction counterclockwise:
+
+      for (int j = 0; j < 10; j++) { //Numero de veces que revisas las variables mientras corre el motor
+        // check variables
+        for (int i = 0; i < ((stepsPerRevolution / j)*actualVolume); i++) { //Divides el Stepsperrevolution por la J que son las veces que quieres revisar las variables.
+          // Move the motor
+          digitalWrite(stepPin, HIGH);
+          delayMicroseconds(2000);
+          digitalWrite(stepPin, LOW);
+          delayMicroseconds(2000);
+        }
+        Serial.print("Motor fase: ");
+        Serial.println(j);
       }
-      digitalWrite(led2, LOW);
+
       state = 0;
       break;
 
@@ -148,4 +142,26 @@ void loop() {
 
 
 
+}
+
+void checkVariables() {
+
+  // Read the value of 3 parameters
+  valuepot1 = analogRead(pot1);
+  valuepot2 = analogRead(pot2);
+  valuepot3 = analogRead(pot3);
+  endstopperValue = digitalRead(endstopper);
+
+  actualVolume = map(valuepot1, 20, 1000, volumeMin, volumeMax);
+  actualCicles = map(valuepot2, 20, 1000, ciclesMin, ciclesMax);
+  actualSpeed = map(valuepot3, 20, 1000, speedMin, speedMax);
+
+  Serial.print("Volume: ");
+  Serial.print(actualVolume);
+  Serial.print(" |  Cicles: ");
+  Serial.print(actualCicles);
+  Serial.print(" |  Speed: ");
+  Serial.print(actualSpeed);
+  Serial.print(" |  Endstopper: ");
+  Serial.println(endstopperValue);
 }
